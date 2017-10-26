084c53e7fa4c52deeffed5c9cd92d8294ddfacbd
s-ballw <s-ballw@54S50012556.it.bsd405.org>
2017-10-26 16:24:56 -0700

    nice

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-import javax.swing.JFrame;////////////////
-public class Main {///////////////////////
-public static void main(String[] args) {//
-JFrame w = createWindow();////////////////
-}/////////////////////////////////////////
-static JFrame createWindow() {////////////
+				import javax.swing.JFrame;
+ 					   public class Main {
+  public static void main(String[] args) {
+	  			JFrame w = createWindow();
+										 }
+  			static JFrame createWindow() {
 JFrame window = new JFrame("Tic-Tac-Toe");
-window.setSize(600, 600);/////////////////
-window.setVisible(true);//////////////////
-return window;////////////////////////////
-}/////////////////////////////////////////
-}/////////////////////////////////////////
+				 window.setSize(600, 600);
+				  window.setVisible(true);
+				  			return window;
+  										 }
+ 					   					 }
 
 /*
 
