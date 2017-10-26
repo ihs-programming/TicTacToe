bf16a9e704ace064184193c3ce6f2d149bc45d88
Peachball <chenyexu@gmail.com>
2017-10-25 21:09:45 -0700

    Init

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame w = createWindow();
+	}
+	
+	static JFrame createWindow() {
+		JFrame window = new JFrame("Tic-Tac-Toe");
+		window.setSize(600, 600);
+		window.setVisible(true);
+		return window;
+	}
+}
