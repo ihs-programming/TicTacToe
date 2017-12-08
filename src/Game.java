5b4571d6e2cb5555e110610c0d66bb5224ef420f
s-yangmi <s-yangmi@54S50011886.it.bsd405.org>
2017-12-07 16:29:20 -0800

    drawing

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,8 @@
 import java.awt.Graphics;
 
 public class Game {
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 800;
 	private TicTacToeBoard b;
 	public Game() {
 		b = new TicTacToeBoard();
@@ -12,7 +14,21 @@ public class Game {
 	 * @param g
 	 */
 	public void draw(Graphics g) {
-		
+		int x, y;
+		for(x = 0; x < 3; x++) {
+			for(y = 0; y < 3; y++) {
+				switch(b.board[x][y]) {
+				case 1: //o
+					g.drawOval(x * WINDOW_WIDTH/3, y * WINDOW_HEIGHT/3, WINDOW_WIDTH/3, WINDOW_HEIGHT/3);
+					break;
+				case 2: //x
+					g.drawLine(x * WINDOW_WIDTH/3, y * WINDOW_HEIGHT/3, (x + 1) * WINDOW_WIDTH/3, (y + 1) * WINDOW_HEIGHT/3);
+					break;
+				}
+				g.drawLine(0, y * WINDOW_HEIGHT/3, WINDOW_WIDTH, y * WINDOW_HEIGHT/3);
+			}
+			g.drawLine(x * WINDOW_WIDTH/3, 0, x * WINDOW_WIDTH/3, WINDOW_HEIGHT);
+		}
 	}
 	
 	public void play(int x, int y) {
